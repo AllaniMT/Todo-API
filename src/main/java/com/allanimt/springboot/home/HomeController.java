6fd78f08e186792c73a2ac59584a19939c942b4c
AllaniMT <52576592+AllaniMT@users.noreply.github.com>
2020-07-07 18:45:44 +0200

    Hello world with Spring Boot

---
+++ src/main/java/com/allanimt/springboot/home/HomeController.java
@@ -0,0 +1,13 @@
+package com.allanimt.springboot.home;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String helloWorldMethod()
+    {
+        return "Hello World :D";
+    }
+}
